670cb9b99367a8d85ac76f1a99a983c8eac5ff8e
tothdomi22 <tothdomi22@gmail.com>
2025-11-19 21:53:48 +0100

    added repositories

---
+++ src/main/java/com/dominik/crafthub/maincategory/repository/MainCategoryRepository.java
@@ -0,0 +1,7 @@
+package com.dominik.crafthub.maincategory.repository;
+
+import com.dominik.crafthub.maincategory.entity.MainCategoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MainCategoryRepository extends JpaRepository<MainCategoryEntity, Integer> {
+}
