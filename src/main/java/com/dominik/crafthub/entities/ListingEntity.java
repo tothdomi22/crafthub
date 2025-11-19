1aa95e35fe9c7c649531f76c5f09619adc2b06a4
tothdomi22 <tothdomi22@gmail.com>
2025-11-19 21:41:52 +0100

    added entities

---
+++ src/main/java/com/dominik/crafthub/entities/ListingEntity.java
@@ -0,0 +1,46 @@
+package com.dominik.crafthub.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "listing")
+public class ListingEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private Integer price;
+
+    @Column(name = "shippable")
+    private Boolean shippable = false;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "status")
+    private String status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity userEntity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sub_category_id")
+    private SubCategoryEntity subCategoryEntity;
+
+    @OneToMany(mappedBy = "listingEntity")
+    private Set<ReviewEntity> reviewEntities = new LinkedHashSet<>();
+
+}
