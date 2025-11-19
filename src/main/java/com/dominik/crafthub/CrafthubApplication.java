d820a693b75543b0c448f57586796b6a49e95cab
tothdomi22 <tothdomi22@gmail.com>
2025-11-19 20:06:55 +0100

    init

---
+++ src/main/java/com/dominik/crafthub/CrafthubApplication.java
@@ -0,0 +1,13 @@
+package com.dominik.crafthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrafthubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrafthubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dominik/crafthub/CrafthubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dominik.crafthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrafthubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
