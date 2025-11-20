3ab852a5633ed655ddd174d59468fcd8d4c166cb
tothdomi22 <tothdomi22@gmail.com>
2025-11-20 21:02:11 +0100

    get me endpoint

--- src/main/java/com/dominik/crafthub/user/controller/UserController.java
+++ src/main/java/com/dominik/crafthub/user/controller/UserController.java
@@ -7,6 +7,7 @@ import com.dominik.crafthub.user.dto.UserDto;
 import com.dominik.crafthub.user.dto.UserLoginRequest;
 import com.dominik.crafthub.user.dto.UserRegisterRequest;
 import com.dominik.crafthub.user.exceptions.UserAlreadyExistsException;
+import com.dominik.crafthub.user.mapper.UserMapper;
 import com.dominik.crafthub.user.repository.UserRepository;
 import com.dominik.crafthub.user.service.UserService;
 import jakarta.servlet.http.Cookie;
@@ -18,6 +19,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
@@ -29,6 +31,7 @@ public class UserController {
   private final UserRepository userRepository;
   private final JwtService jwtService;
   private final JwtConfig jwtConfig;
+  private final UserMapper userMapper;
 
   @PostMapping
   public ResponseEntity<UserDto> register(@RequestBody @Valid UserRegisterRequest request) {
@@ -49,6 +52,18 @@ public class UserController {
     cookie.setHttpOnly(true);
     response.addCookie(cookie);
     return ResponseEntity.status(HttpStatus.OK).build();
+  }
+
+  @GetMapping("/me")
+  public ResponseEntity<?> me() {
+    var authentication = SecurityContextHolder.getContext().getAuthentication();
+    var userId = (Long) authentication.getPrincipal();
+    var user = userRepository.findById(userId).orElse(null);
+    if (user == null) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+    var userDto = userMapper.toDto(user);
+    return ResponseEntity.status(HttpStatus.OK).body(userDto);
   }
 
   @ExceptionHandler(UserAlreadyExistsException.class)
