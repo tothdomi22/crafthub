d14aac32a590ef31f2ffc7bd4af1a4170a508b77
tothdomi22 <tothdomi22@gmail.com>
2025-11-20 22:06:35 +0100

    email validation

--- src/main/java/com/dominik/crafthub/user/dto/UserRegisterRequest.java
+++ src/main/java/com/dominik/crafthub/user/dto/UserRegisterRequest.java
@@ -1,8 +1,10 @@
 package com.dominik.crafthub.user.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record UserRegisterRequest(
     @NotBlank(message = "Must provide a name!") String name,
-    @NotBlank(message = "Must provide an email!") String email,
+    @NotBlank(message = "Must provide an email!") @Email(message = "Must be a valid email!")
+        String email,
     @NotBlank(message = "Must provide a password!") String password) {}
