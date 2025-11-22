b26fe72d4adda692c019d900f99facaf7bc416ef
tothdomi22 <tothdomi22@gmail.com>
2025-11-22 10:24:46 +0100

    added dto

---
+++ src/main/java/com/dominik/crafthub/listing/dto/ListingCreateRequest.java
@@ -0,0 +1,11 @@
+package com.dominik.crafthub.listing.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record ListingCreateRequest(
+    @NotBlank(message = "You must provide a name") String name,
+    @NotNull(message = "You must provide a price") Integer price,
+    @NotNull(message = "You must device if you can ship or not") boolean canShip,
+    @NotBlank(message = "You must provide a city") String city,
+    @NotNull(message = "You must provide a subcategory") Integer subCategoryId) {}
