594e9f2943bc97528170c9a6b1482b22bf189fee
tothdomi22 <tothdomi22@gmail.com>
2025-11-23 22:13:57 +0100

    get main category api calls

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -1,9 +1,18 @@
+"use client";
+
 import React from "react";
+import {useQuery} from "@tanstack/react-query";
+import useListMainCategory from "@/app/hooks/admin/main-category/useListMainCategory";
 
 export default function AdminPage() {
+  const {data: mainCategoriesData} = useQuery({
+    queryFn: useListMainCategory,
+    queryKey: ["mainCategories"],
+  });
+  console.log(mainCategoriesData);
   return (
     <div>
-      <p>admin page</p>
+      <p>main categories</p>
     </div>
   );
 }
