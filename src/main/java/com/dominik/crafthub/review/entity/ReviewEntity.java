9e3b5bb81cc974db9221e92a79c44e4c83ccbaeb
tothdomi22 <tothdomi22@gmail.com>
2025-12-12 18:25:35 +0100

    update review entity

--- src/main/java/com/dominik/crafthub/review/entity/ReviewEntity.java
+++ src/main/java/com/dominik/crafthub/review/entity/ReviewEntity.java
@@ -1,39 +1,44 @@
 package com.dominik.crafthub.review.entity;
 
 import com.dominik.crafthub.listing.entity.ListingEntity;
+import com.dominik.crafthub.purchaserequest.entity.PurchaseRequestEntity;
 import com.dominik.crafthub.user.entity.UserEntity;
 import jakarta.persistence.*;
 import java.time.OffsetDateTime;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.ColumnDefault;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "review")
 public class ReviewEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private Long id;
 
-    @ColumnDefault("1")
-    @Column(name = "stars")
-    private Short stars;
+  @Column(name = "stars")
+  private Short stars;
 
-    @Column(name = "review_text")
-    private String reviewText;
-    
-    @Column(name = "created_at")
-    private OffsetDateTime createdAt;
+  @Column(name = "review_text")
+  private String reviewText;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "listing_id")
-    private ListingEntity listingEntity;
+  @Column(name = "created_at")
+  private OffsetDateTime createdAt;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "reviewer_user_id")
-    private UserEntity reviewerUserEntity;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "listing_id")
+  private ListingEntity listingEntity;
 
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "reviewer_user_id")
+  private UserEntity reviewerUserEntity;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "purchase_request_id")
+  private PurchaseRequestEntity purchaseRequestEntity;
+
+  @Enumerated(EnumType.STRING)
+  private ReviewTypeEnum reviewType;
 }
